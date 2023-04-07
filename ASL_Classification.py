e3095e5e33378b829e99760b2ea9e4e6c82d70e2
unknown <shayna.goles31@gmail.com>
2023-04-07 08:10:04 +0800

    changes

--- ASL_Classification.py
+++ ASL_Classification.py
@@ -29,7 +29,7 @@ with tabs[0]:
         #               1   2   3   4   5   6   7   8   9      10  11  12  13  14  15  16  17  18  19  20  21  22  23  24 
     class_names = ['A','B','C','D','E','F','G','H','I','','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y']
     ##########################################################################################
-    uploaded_files = st.file_uploader("", type=["jpg"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader("", type=["jpg","JPEG"], accept_multiple_files=True)
     if uploaded_files or st.session_state.load_state:
         st.session_state.load_state = True
         if uploaded_files:
